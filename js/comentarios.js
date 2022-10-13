d8105a9c7c4cd84bfd8340a99559d0abadba3f07
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2022-10-13 20:12:43 -0300

    Executando explicações

---
+++ js/comentarios.js
@@ -0,0 +1,10 @@
+// Como comentar linha de code.
+
+/*
+
+Como comentar bloco
+de code
+
+*/
+
+console.log("Starting")
---
+++ praticando-avancando/scripts.js
@@ -0,0 +1,63 @@
+// Object
+
+// const person = {
+//     name: "Jonh",
+//     age: 30,
+//     weight: 88.5,
+//     isAdmin: true,
+// }
+
+// console.log(person.name)
+
+
+// --------------------------------- --------------------------------- --------------------------------- 
+// var weight = 67 // 1
+// 2 tipo undefined
+
+// 3
+// let name = "Jonh";
+// let age = 30;
+// let stars = 35.5;
+// let isSubscribed = true;
+
+// 4
+// tipo object
+
+//4.1
+const student = {
+    name: "Jonh",
+    age: 30,
+    weight: 63.5,
+    isSubscribed: true,
+}
+
+// 4.2
+// console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg`)
+
+// 5
+let students = []
+
+// 6
+students = [student]
+// console.log(students)
+
+// 7
+// console.log(students[0])
+
+// 8
+// const miguel = {
+//     name: "Miguel",
+//     age: 21,
+//     weight: 63.5,
+//     isSubscribed: true,
+// }
+
+// students[1] = miguel
+// console.log(students)
+
+// 9
+// A resposta é que o var a será undefined, pois ele foi atribuida depois do console.log que tem no code, isso se chama hosting
+
+
+
+
