1c09b713a1c50acee653884c08bcc78ed801e391
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2023-01-10 00:19:53 -0300

    Created require test

---
+++ NodeJs/require.js
@@ -0,0 +1,9 @@
+// Modules native
+// const path = require("path");
+// console.log(path.basename(__filename));
+
+// My Module
+
+const myModule = require('./exports.js');
+
+console.log(myModule);
